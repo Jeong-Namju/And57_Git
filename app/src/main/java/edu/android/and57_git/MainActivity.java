f50dcc782e32e567ef87b0f7493912028b8c45c0
Jeong-Namju <zekallyang@naver.com>
2017-09-14 11:16:56 +0900

    aaa TextView 수정

--- app/src/main/java/edu/android/and57_git/MainActivity.java
+++ app/src/main/java/edu/android/and57_git/MainActivity.java
@@ -2,13 +2,17 @@ package edu.android.and57_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
 
     }
 
@@ -20,7 +24,4 @@ public class MainActivity extends AppCompatActivity {
         // developer aaa
     }
 
-    public void testBBB() {
-        // 개발자 bbb
-    }
 }
